27af07a9d55e9d9b517c45db3ee19ef4b0941fb2
jnt <ava_tk@126.com>
2026-08-02 19:35:43 +0800

    chore: 清理误提交的构建产物 + gitignore 补 crates 内模式

--- crates/stitch-desktop/frontend/build/_app/immutable/entry/start.TLOUB8ZQ.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DQBT6u68.js";export{o as load_css,r as start};
--- crates/stitch-desktop/frontend/build/_app/immutable/nodes/2.NqI_SpP0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,f as n}from"../chunks/DGuj__yr.js";export{e as component,n as universal};
